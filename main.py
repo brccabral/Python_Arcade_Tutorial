7a1e57ed5337f14a674a1a497c4a1cc70d1a3d61
Bruno Cabral <cabral.brc@gmail.com>
2022-06-01 11:36:09 -0700

    mouse interactions

--- main.py
+++ main.py
@@ -8,76 +8,46 @@ class MyGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.circle_x = 100
-        self.circle_y = 100
-        self.circle_r = 50
-        self.x_speed = 300
-        self.y_speed = 150
+        self.green_x = 100
+        self.green_y = 100
 
-        self.player_x = 100
-        self.player_y = 200
-        self.player_speed = 250
-        self.right = False
-        self.left = False
-        self.up = False
-        self.down = False
+        self.blue_x = 300
+        self.blue_y = 300
+
+        self.yellow_x = 500
+        self.yellow_y = 500
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_circle_filled(
-            self.circle_x,
-            self.circle_y,
-            self.circle_r,
+            self.green_x,
+            self.green_y,
+            50,
             arcade.color.GREEN,
-            num_segments=10,
+            num_segments=20,
+        )
+        arcade.draw_circle_filled(
+            self.blue_x,
+            self.blue_y,
+            50,
+            arcade.color.BLUE,
+            num_segments=20,
         )
         arcade.draw_circle_outline(
-            self.player_x, self.player_y, 50, arcade.color.BLUE, 2, num_segments=20
+            self.yellow_x, self.yellow_y, 50, arcade.color.YELLOW, 2, num_segments=20
         )
 
-    def on_update(self, delta_time):
-        self.circle_x += self.x_speed * delta_time
-        self.circle_y += self.y_speed * delta_time
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            self.green_x = x
+            self.green_y = y
+        if button == arcade.MOUSE_BUTTON_RIGHT:
+            self.blue_x = x
+            self.blue_y = y
 
-        if (
-            self.circle_x > self.width - self.circle_r
-            or self.circle_x < 0 + self.circle_r
-        ):
-            self.x_speed *= -1
-        if (
-            self.circle_y > self.height - self.circle_r
-            or self.circle_y < 0 + self.circle_r
-        ):
-            self.y_speed *= -1
-
-        if self.right:
-            self.player_x += self.player_speed * delta_time
-        if self.left:
-            self.player_x -= self.player_speed * delta_time
-        if self.up:
-            self.player_y += self.player_speed * delta_time
-        if self.down:
-            self.player_y -= self.player_speed * delta_time
-
-    def on_key_press(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.RIGHT:
-            self.right = True
-        if symbol == arcade.key.LEFT:
-            self.left = True
-        if symbol == arcade.key.UP:
-            self.up = True
-        if symbol == arcade.key.DOWN:
-            self.down = True
-
-    def on_key_release(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.RIGHT:
-            self.right = False
-        if symbol == arcade.key.LEFT:
-            self.left = False
-        if symbol == arcade.key.UP:
-            self.up = False
-        if symbol == arcade.key.DOWN:
-            self.down = False
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        self.yellow_x = x
+        self.yellow_y = y
 
 
 MyGameWindow(1280, 720, "My Game Window")
