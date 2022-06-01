4c6e809ade130a03be7c380809eae5cfe3c946c0
Bruno Cabral <cabral.brc@gmail.com>
2022-06-01 11:01:27 -0700

    draw some shapes

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class MyGameWindow(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_lines([(0, 0), (100, 100)], arcade.color.RED, 4)
+        arcade.draw_point(150, 150, arcade.color.WHITE, 20)
+        arcade.draw_circle_filled(250, 150, 50, arcade.color.GREEN, 50)
+        arcade.draw_circle_outline(450, 200, 50, arcade.color.BLUE, 5, 50)
+        arcade.draw_lrtb_rectangle_filled(500, 600, 600, 500, arcade.color.YELLOW)
 
 
 MyGameWindow(1280, 720, "My Game Window")
