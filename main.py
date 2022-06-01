fb7c84ad08cf0d9c257c8e287081944b1c5f3c9e
Bruno Cabral <cabral.brc@gmail.com>
2022-06-01 11:08:38 -0700

    circle moves to the right

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ class MyGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        self.circle_x = 100
+        self.circle_y = 100
+        self.x_speed = 300
+        self.y_speed = 150
+
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_lines([(0, 0), (100, 100)], arcade.color.RED, 4)
-        arcade.draw_point(150, 150, arcade.color.WHITE, 20)
-        arcade.draw_circle_filled(250, 150, 50, arcade.color.GREEN, 50)
-        arcade.draw_circle_outline(450, 200, 50, arcade.color.BLUE, 5, 50)
-        arcade.draw_lrtb_rectangle_filled(500, 600, 600, 500, arcade.color.YELLOW)
+        arcade.draw_circle_filled(
+            self.circle_x, self.circle_y, 50, arcade.color.GREEN, num_segments=10
+        )
+
+    def on_update(self, delta_time):
+        self.circle_x += self.x_speed * delta_time
+        self.circle_y += self.y_speed * delta_time
 
 
 MyGameWindow(1280, 720, "My Game Window")
