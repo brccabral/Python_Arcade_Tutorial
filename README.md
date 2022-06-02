97767e48a6abc8a3328caf2c32780d6c1f9ff017
Bruno Cabral <cabral.brc@gmail.com>
2022-06-01 21:25:59 -0700

    show sprite on screen

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python Arcade Tutorial
+
+Tutorial from AtiByte https://www.youtube.com/watch?v=LQ3r36uyHNY  
+
+Space ship from "Branden Hughes" https://opengameart.org/content/little-blue-spaceship under CC0  
+
--- main.py
+++ main.py
@@ -8,64 +8,50 @@ class MyGameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.green_x = 100
-        self.green_y = 100
+        self.player_x = 100
+        self.player_y = 200
+        self.player_speed = 250
 
-        self.blue_x = 300
-        self.blue_y = 300
+        self.sprite1 = arcade.Sprite("ship1a.png", center_x=100, center_y=100)
 
-        self.yellow_x = 500
-        self.yellow_y = 500
-        self.velocity_x = 0
-        self.velocity_y = 0
+        self.right = False
+        self.left = False
+        self.up = False
+        self.down = False
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_circle_filled(
-            self.green_x,
-            self.green_y,
-            50,
-            arcade.color.GREEN,
-            num_segments=20,
-        )
-        arcade.draw_circle_filled(
-            self.blue_x,
-            self.blue_y,
-            50,
-            arcade.color.BLUE,
-            num_segments=20,
-        )
-        arcade.draw_circle_outline(
-            self.yellow_x, self.yellow_y, 50, arcade.color.YELLOW, 2, num_segments=20
-        )
+        self.sprite1.draw()
 
     def on_update(self, delta_time: float):
-        self.move_yellow_circle()
+        if self.right:
+            self.player_x += self.player_speed * delta_time
+        if self.left:
+            self.player_x -= self.player_speed * delta_time
+        if self.up:
+            self.player_y += self.player_speed * delta_time
+        if self.down:
+            self.player_y -= self.player_speed * delta_time
 
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.green_x = x
-            self.green_y = y
-        if button == arcade.MOUSE_BUTTON_RIGHT:
-            self.blue_x = x
-            self.blue_y = y
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.RIGHT:
+            self.right = True
+        if symbol == arcade.key.LEFT:
+            self.left = True
+        if symbol == arcade.key.UP:
+            self.up = True
+        if symbol == arcade.key.DOWN:
+            self.down = True
 
-    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
-        self.velocity_x = x
-        self.velocity_y = y
-
-    def move_yellow_circle(self):
-        x_distance = self.velocity_x - self.yellow_x
-        y_distance = self.velocity_y - self.yellow_y
-        # distance = sqrt(x_distance * x_distance + y_distance * y_distance)
-        # pow(a, 0.5) is more efficient than sqrt()
-        # distance = pow(x_distance * x_distance + y_distance * y_distance, 0.5)
-        # a**.5 is even faster
-        distance = (x_distance * x_distance + y_distance * y_distance)**.5
-
-        if distance > 1:
-            self.yellow_x += x_distance * 0.1
-            self.yellow_y += y_distance * 0.1
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.RIGHT:
+            self.right = False
+        if symbol == arcade.key.LEFT:
+            self.left = False
+        if symbol == arcade.key.UP:
+            self.up = False
+        if symbol == arcade.key.DOWN:
+            self.down = False
 
 
 MyGameWindow(1280, 720, "My Game Window")
