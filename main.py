1b116c0f1fd472a228a6a316afb93865a605dc49
Bruno Cabral <cabral.brc@gmail.com>
2022-06-09 15:47:50 -0700

    camera follows player

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ GRAVITY = 1.1
 
 # map
 TILE_WIDTH = 64
+HALF_TILE_WIDTH = TILE_WIDTH // 2
 MAP_WIDTH = 60 * TILE_WIDTH
 MAP_HEIGHT = 6 * TILE_WIDTH
+MAX_PLAYER_POS_X = MAP_WIDTH - TILE_WIDTH // 2
 
 # window
 WINDOW_WIDTH = 1280
@@ -29,7 +31,9 @@ class MyGameWindow(arcade.Window):
         self.player_sprite.center_y = TILE_WIDTH * 2
         self.player_list.append(self.player_sprite)
         self.physics_engine = None
-        self.camera = None
+        self.camera = arcade.Camera(self.width, self.height)
+        # camera to draw HUD
+        self.gui_camera = arcade.Camera(self.width, self.height)
 
         self.collected_coins = 0
 
@@ -46,18 +50,51 @@ class MyGameWindow(arcade.Window):
 
     def on_draw(self):
         self.clear()
+
+        # activate camera to draw scene on it
+        self.camera.use()
+
         self.scene.draw()
+
+        # after gui_camera.use(), arcade will draw anything on this camera, not in the other camera
+        #  otherwise the text will stay at the map start
+        self.gui_camera.use()
         arcade.draw_text(
             f"Coins: {self.collected_coins}",
-            arcade.get_viewport()[0] + 10,
-            arcade.get_viewport()[2] + WINDOW_HEIGHT - 50,
+            10,
+            WINDOW_HEIGHT - 50,
             arcade.color.GOLD,
             font_size=30,
         )
 
+    def clamp(self, value, low, high):
+        return max(min(value, high), low)
+
+    def camera_position(self):
+        # constrain player inside map boundaries
+        self.player_sprite.center_x = self.clamp(
+            self.player_sprite.center_x, HALF_TILE_WIDTH, MAX_PLAYER_POS_X
+        )
+
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+
+        if MAP_WIDTH - self.player_sprite.center_x < self.camera.viewport_width / 2:
+            screen_center_x = MAP_WIDTH - self.camera.viewport_width
+
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
     def on_update(self, delta_time: float):
         self.physics_engine.update()
-        # self.camera_position()
+        self.camera_position()
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
